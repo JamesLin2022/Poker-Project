e8206cc2d6eabe14ede1eeb5543332e440361922
JamesLin2022 <james.09461@gmail.com>
2023-11-18 18:08:33 -0800

    adding layout

--- root_queries.sql
+++ root_queries.sql
@@ -1,7 +1,7 @@
 CREATE USER 'poker_user'@'%' IDENTIFIED BY 'my_cool_password';
 CREATE USER 'poker_user'@'localhost' IDENTIFIED BY 'my_cool_password';
 CREATE DATABASE pokerdb;
-USE definitionsdb;
+USE pokerdb;
 
 GRANT ALL PRIVILEGES ON pokerdb.* TO 'poker_user'@'%' IDENTIFIED BY 'my_cool_password';
 GRANT ALL PRIVILEGES ON pokerdb.* TO 'poker_user'@'localhost' IDENTIFIED BY 'my_cool_password';
